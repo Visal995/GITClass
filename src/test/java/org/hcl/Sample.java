b4974ceaa6695088416d55f0814beda2480eb1e0
GITHUB <visalkumar0901@gmail.com>
2022-09-17 19:23:03 +0530

    message

---
+++ src/test/java/org/hcl/Sample.java
@@ -0,0 +1,21 @@
+package org.hcl;
+
+public class Sample {
+
+	public void method1() {
+		System.out.println("method1");
+
+	}
+	
+	public void method2() {
+	System.out.println("method2");
+
+	}
+	public void method3() {
+		System.out.println("method3");
+
+		}
+	
+	
+	
+}
